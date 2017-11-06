fd7a7e79268b2943754e928050ea5e6401fb078f
Juliiii <zhulf6@mail2.sysu.edu.cn>
2017-11-06 18:20:58 +0800

    try some components

---
+++ src/utils/px2dp.js
@@ -0,0 +1,15 @@
+import {
+	Dimensions
+} from 'react-native';
+
+const uiheight = 640;
+const physicialheight = Dimensions.get('window').height;
+
+function px2dp(px) {
+    return physicialheight * px/ uiheight;
+}
+
+
+export {
+	px2dp
+};
